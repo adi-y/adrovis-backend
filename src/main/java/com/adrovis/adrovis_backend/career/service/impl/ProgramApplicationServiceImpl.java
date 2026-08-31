cc868a7967479a44c846a15f803b692e26bb4aeb
rohan mandal <146451203+rohanmandal341@users.noreply.github.com>
2026-08-31 23:59:04 +0530

    Update program title from 'Software Developer' to 'Software Developer Internship'

--- src/main/java/com/adrovis/adrovis_backend/career/service/impl/ProgramApplicationServiceImpl.java
+++ src/main/java/com/adrovis/adrovis_backend/career/service/impl/ProgramApplicationServiceImpl.java
@@ -24,7 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ProgramApplicationServiceImpl implements ProgramApplicationService {
 
-    private static final String PROGRAM_TITLE_SNAPSHOT = "Software Developer";
+    private static final String PROGRAM_TITLE_SNAPSHOT = "Software Developer Internship";
 
     private final ApplicationRepository applicationRepository;
     private final FileStorageService fileStorageService;
